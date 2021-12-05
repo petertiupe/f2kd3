4a1df011f1a389c57ffc50c1df264fc62865e183
peter <peter@tiupe.de>
2021-12-05 17:51:35 +0100

    template and example5 added as preparation

--- src/jsMain/kotlin/app.kt
+++ src/jsMain/kotlin/app.kt
@@ -6,6 +6,9 @@ fun main() {
    if(window.location.href=="http://localhost:8080/example2.html") example2()
    if(window.location.href=="http://localhost:8080/example3.html") example3()
    if(window.location.href=="http://localhost:8080/example4.html") example4()
+   if(window.location.href=="http://localhost:8080/example5.html") example5()
+
+   if(window.location.href=="http://localhost:8080/template.html") template()
 
 }
 
---
+++ src/jsMain/kotlin/example5.kt
@@ -0,0 +1,16 @@
+import de.tiupe.Selection
+import de.tiupe.d3
+import dev.fritz2.binding.Store
+import dev.fritz2.binding.storeOf
+import model.Framework
+import org.w3c.dom.events.Event
+import kotlin.reflect.typeOf
+
+
+
+fun example5() {
+   console.log("Example5 is running")
+
+}
+
+
